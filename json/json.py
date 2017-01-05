84c45e991c688fbf4c32ad17733b165454966710
邱奕玮 <tomhaoye@163.com>
2017-01-05 14:09:02 +0800

    practice

---
+++ json/json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import demjson
+
+data = [{'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}]
+
+json = demjson.encode(data)
+print json
+
+json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+
+text = demjson.decode(json)
+print text
